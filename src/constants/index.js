4a45c7f1347950f65a0268b825ce1fbc88d09f21
Arjunsharmahehe <whocaresaboutarjun@gmail.com>
2025-07-14 22:03:21 +0530

    cleanup

---
+++ src/constants/index.js
@@ -0,0 +1,26 @@
+export const utilities = [
+    {
+        id: 1,
+        name: "Lorem Ipsum",
+        description: "Generate placeholder text for your projects.",
+        route: "/lorem-ipsum"
+    },
+    {
+        id: 2,
+        name: "JWT Decoder",
+        description: "Decode and verify JSON Web Tokens.",
+        route: "/jwt-decoder"
+    },
+    {
+        id: 3,
+        name: "Base64 Encoder/Decoder",
+        description: "Encode and decode Base64 strings.",
+        route: "/base64-encoder-decoder"
+    },
+    {
+        id: 4,
+        name: "UUID Generator",
+        description: "Generate UUIDs for unique identifiers.",
+        route: "/uuid-generator"
+    }
+]
