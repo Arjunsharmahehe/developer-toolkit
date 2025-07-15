109203e90288523265d9775d9dfd3e387480d71c
Arjunsharmahehe <whocaresaboutarjun@gmail.com>
2025-07-15 21:30:51 +0530

    fix the footer margin issue

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import { ExternalLink } from "lucide-react";
 export default function Footer() {
     return (
         <>
-            <footer className="w-full max-w-[500px] mx-auto flex text-sm text-neutral-400 justify-between items-baseline">
+            <footer className="w-full max-w-[500px] mx-auto mt-4 flex text-sm text-neutral-400 justify-between items-baseline">
                 <p>developer-toolkit</p>
                 <Link className="flex gap-1 items-center hover:underline"
                     href={"https://github.com/arjunsharmahehe/developer-toolkit"}>
